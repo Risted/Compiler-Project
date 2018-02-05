727e8b13f57f5c163f0451e2e4f6e68fbb44143a
Risted <riste15@student.sdu.dk>
2018-02-05 10:48:07 +0100

    test

---
+++ sources/symbol.c
@@ -0,0 +1,25 @@
+#include "symbol.h"
+
+int Hash(char *str){
+
+}
+
+SymbolTable *initSymbolTable(){
+
+}
+
+SymbolTable *scopeSymbolTable(SymbolTable *t){
+
+}
+
+SYMBOL *putSymbol(SymbolTable *t, char *name, int value){
+
+}
+
+SYMBOL *getSymbol(SymbolTable *t, char *name){
+
+}
+
+void dumpSymbolTable(SymbolTable *t){
+  
+}
