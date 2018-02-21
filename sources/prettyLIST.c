71c1e4cc192132752d657d18714db85a4d92bcfc
Obelicks <bloechrr@gmail.com>
2018-02-21 15:32:29 +0100

    compiles

--- sources/prettyLIST.c
+++ sources/prettyLIST.c
@@ -4,8 +4,13 @@
 void prettyLIST(LIST *l){
   switch (l->kind) {
     case parK:
+      printf("parK\n");
       prettyLIST(l->val.parL);
       break;
-
+    case varlistK:
+      printf("varlistK\n");
+      prettyTYPE(l->val.varlistL.var_type);
+      prettyLIST(l->val.varlistL.var_decl_list);
+      break;
   }
 }
