bcd0be0d1bc5024bccaf97e950a9065d80fb3ec5
Obelicks <bloechrr@gmail.com>
2018-03-04 14:36:11 +0100

    iteration added

--- sources/typecheck.c
+++ sources/typecheck.c
@@ -16,10 +16,10 @@ SymbolTable* typeCheck(FUNC* body){
 }
 
 int deepTypeCheck(FUNC *function){
-  LIST *var_decl_list = function->val.bodyF.decl_list;
+  LIST *decl_list = function->val.bodyF.decl_list;
   LIST *stm_list = function->val.bodyF.statement_list;
   DEC declaration;
-  while (var_decl_list != NULL) {
+  while (decl_list != NULL) {
     declaration = decl_list->val.decL.declaration;
     //type: tID\n tINT\n tARRAY tOF type\n tRECORD tOF tLBRACE var_decl_list tRBRACE \n
     //declaration : tTYPE tID tASSIGN type tSEMI\n function\n tVAR var_decl_list tSEMI\n
@@ -28,6 +28,7 @@ int deepTypeCheck(FUNC *function){
       //like this: int a = 42, string b = a
       //but i cant seem to see where in the code i fix this =(
     }
+    decl_list = decl_list->val.decL.decl_list;
   }
   return 0;
 }
